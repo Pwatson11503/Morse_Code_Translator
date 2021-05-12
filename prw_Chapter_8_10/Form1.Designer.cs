53fdb5921a77ecdcd5d8891c8bd07842bdbe089c
Preston Watson <prw20@txstate.edu>
2021-05-12 16:41:46 -0500

    Add project files.

---
+++ prw_Chapter_8_10/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace prw_Chapter_8_10
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.txtInput = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.lblOutput = new System.Windows.Forms.Label();
+			this.btnTranslate = new System.Windows.Forms.Button();
+			this.btnExit = new System.Windows.Forms.Button();
+			this.btnClear = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// txtInput
+			// 
+			this.txtInput.Location = new System.Drawing.Point(101, 25);
+			this.txtInput.Name = "txtInput";
+			this.txtInput.Size = new System.Drawing.Size(100, 20);
+			this.txtInput.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(87, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(126, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Translate to Morse Code!";
+			// 
+			// lblOutput
+			// 
+			this.lblOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblOutput.Location = new System.Drawing.Point(12, 48);
+			this.lblOutput.Name = "lblOutput";
+			this.lblOutput.Size = new System.Drawing.Size(304, 244);
+			this.lblOutput.TabIndex = 2;
+			// 
+			// btnTranslate
+			// 
+			this.btnTranslate.Location = new System.Drawing.Point(32, 324);
+			this.btnTranslate.Name = "btnTranslate";
+			this.btnTranslate.Size = new System.Drawing.Size(75, 23);
+			this.btnTranslate.TabIndex = 3;
+			this.btnTranslate.Text = "Translate";
+			this.btnTranslate.UseVisualStyleBackColor = true;
+			this.btnTranslate.Click += new System.EventHandler(this.btnTranslate_Click);
+			// 
+			// btnExit
+			// 
+			this.btnExit.Location = new System.Drawing.Point(194, 324);
+			this.btnExit.Name = "btnExit";
+			this.btnExit.Size = new System.Drawing.Size(75, 23);
+			this.btnExit.TabIndex = 4;
+			this.btnExit.Text = "Exit";
+			this.btnExit.UseVisualStyleBackColor = true;
+			this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+			// 
+			// btnClear
+			// 
+			this.btnClear.Location = new System.Drawing.Point(113, 324);
+			this.btnClear.Name = "btnClear";
+			this.btnClear.Size = new System.Drawing.Size(75, 23);
+			this.btnClear.TabIndex = 5;
+			this.btnClear.Text = "Clear";
+			this.btnClear.UseVisualStyleBackColor = true;
+			this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(329, 363);
+			this.Controls.Add(this.btnClear);
+			this.Controls.Add(this.btnExit);
+			this.Controls.Add(this.btnTranslate);
+			this.Controls.Add(this.lblOutput);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.txtInput);
+			this.Name = "Form1";
+			this.Text = "Morse Code Converter";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox txtInput;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label lblOutput;
+		private System.Windows.Forms.Button btnTranslate;
+		private System.Windows.Forms.Button btnExit;
+		private System.Windows.Forms.Button btnClear;
+	}
+}
+
